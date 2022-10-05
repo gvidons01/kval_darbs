31761ce1a4cb0b763a5e9c11aa24658ea4f5e199
gvido <me@email.com>
2022-10-05 08:53:31 +0300

    adding files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StartComponent } from './start/start.component';
+
+const routes: Routes = [
+  { path: 'start', component: StartComponent },
+  { path: '', redirectTo: '/start', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { StartComponent } from './start/start.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    StartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
