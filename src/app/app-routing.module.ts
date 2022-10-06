cd34dc63f178531e93acff98b6b8dbcbcdb8106e
gvido <me@email.com>
2022-10-06 11:23:02 +0300

    More categories and logos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StartComponent } from './start/start.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   { path: 'start', component: StartComponent },
+  { path: 'search', component: SearchComponent },
   { path: '', redirectTo: '/start', pathMatch: 'full' }
 ];
 
