970f41f032294a7f532906686b706352ed039905
gvido <me@email.com>
2022-11-04 14:29:13 +0200

    modified auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  //Urls
-  authUrl = 'http://localhost:8000/oauth/token';
-  apiUrl = 'http://localhost:8000/api';
-  options: any;
+  private baseUrl = 'http://localhost:8000/api';
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient, public router: Router) { }
+
+  login(username: string, password: string): Observable<boolean>{
+    return this.http.post<{token: string}>(this.baseUrl+'/login', {email: username, password: password})
+    .pipe(
+      map(result=>{
+        localStorage.setItem('access_token', result.token);
+        return true;
+      })
+    );
+  }
+
+  getToken() {
+    return localStorage.getItem('access_token');
+  }
+
+  logout(){
+    let removeToken = localStorage.removeItem('access_token');
+
+    if(removeToken == null){
+      this.router.navigate(['login']);
+    }
+  }
+
+  
 }
